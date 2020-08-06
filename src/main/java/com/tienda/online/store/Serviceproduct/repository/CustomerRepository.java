bc8f7f16b6f624443bacca95e22d276e62c17d57
Alexis <omaroquendo221@gmail.com>
2020-08-06 18:31:15 -0500

    Se agrego la entidad Compra y región, como también el repositorio compra.

---
+++ src/main/java/com/tienda/online/store/Serviceproduct/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.tienda.online.store.Serviceproduct.repository;
+
+import com.tienda.online.store.Serviceproduct.entity.Customer;
+import com.tienda.online.store.Serviceproduct.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+    public Customer findByNumberID(String numberID);
+    public List<Customer> findByLastName(String lastName);
+    public List<Customer> findByRegion(Region region);
+}
